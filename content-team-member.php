82549c425bbe7db3e16aebba814d6a0fa01816b0
Mario Vallejo <mario@vallgroup.com>
2014-11-20 20:32:15 -0600

    added team member wp_query

---
+++ content-team-member.php
@@ -0,0 +1,39 @@
+<?php
+
+$team_members = new WP_Query( array( 'post_type' => 'teammember' ) ); 
+$posts = $team_members->get_posts(); ?>
+
+<div class="bloodhound-accordion border-box" style="overflow:hidden;min-height:450px;"><!-- Beigin Team Member -->
+
+	<?php 
+	foreach ( $posts as $post ) :
+		setup_postdata( $post );
+		
+		bloodhound_the_team_member_name(); ?>
+		<article <?php post_class( 'bloodhound-team-member inline-block' );  ?> <?php bloodhound_the_page_styles(); ?>>
+			
+			<?php if( has_post_thumbnail() ) : ?>
+				<div class="span4 float-left inline-block border-box" style="padding:0 20px 10px 0;">
+					<?php the_post_thumbnail( 'medium', array( 'class' => 'inline-block responsive' ) ); ?>
+				</div>
+			<?php $span = 'span8'; 
+			else : 
+				$span = 'block';
+			endif; ?>
+
+			<div class="bloodhound-team-member-info float-left <?php echo $span; ?>">
+				<?php bloodhound_the_team_meber_info(); ?>
+			</div>
+
+			<div class="clear"></div>
+
+			<div class="block justify">
+				<?php the_content(); ?>
+			</div>
+
+		</article>
+
+	<?php 
+	endforeach; ?>
+
+</div>
