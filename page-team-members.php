256073ba4d34c871995a7ca3576491194b917234
Mario Vallejo <mario@vallgroup.com>
2014-12-13 12:49:40 -0600

    page team members

---
+++ page-team-members.php
@@ -0,0 +1,23 @@
+<?php 
+/* Template Name: Team Members */
+
+get_header(  );
+
+$title = bloodhound_the_title( $post, false, false ); ?>
+
+
+<section id="bloodhound_team_members_page" class="block">
+
+	<?php bloodhound_the_title( $post ); ?>
+
+	<div class="clear"></div>
+
+	<div class="container">
+
+		<?php get_template_part( 'content', 'team-members' ); ?>
+
+	</div>
+
+</section>
+
+<?php get_footer(); ?>
